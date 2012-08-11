ab7558dbba1d8ea68a5de1854ad83b2af3c42ff4
lihe757 <lihe757@gmail.com>
2012-08-11 23:42:43 +0800

    recovery code for precode will crash system

--- AppleAC97AudioDriver/AppleAC97AudioEnginePCMOut.cpp
+++ AppleAC97AudioDriver/AppleAC97AudioEnginePCMOut.cpp
@@ -222,7 +222,7 @@ bool CLASS::initAudioStreams( void )
          i++)
     {
         streamFormat.fNumChannels = config->getAudioChannelCount();
-        streamFormat.fDriverTag   = *((UInt*)(config->getClientData()));
+        streamFormat.fDriverTag   = (IOByteCount)config->getClientData();
         sampleRate.whole          = config->getSampleRate();
         sampleRate.fraction       = 0;
 
@@ -245,7 +245,7 @@ bool CLASS::initAudioStreams( void )
     if (fConfigActive)
     {
         streamFormat.fNumChannels = fConfigActive->getAudioChannelCount();
-        streamFormat.fDriverTag   = *((UInt*)(fConfigActive->getClientData()));
+        streamFormat.fDriverTag   = (IOByteCount)fConfigActive->getClientData();
         fOutputStream->setFormat( &streamFormat );
         DebugLog("%s: set output stream format to %lu-ch, tag %lu\n",
                  getName(), streamFormat.fNumChannels,
